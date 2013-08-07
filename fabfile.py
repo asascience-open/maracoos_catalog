11c4ab39321da9cf5f32aed4c89e71b337985ebd
Kyle Wilcox <kwilcox@asascience.com>
2013-08-07 11:50:16 -0400

    Update deploy script to use spaces not tabs.  Add style deployment

--- fabfile.py
+++ fabfile.py
@@ -5,8 +5,8 @@ from copy import copy
 import time
 
 """
-	Call this with fab -H <hostname> TASK to deploy THREDDS
-	Note: SSH public key must be in /home/thredds/.ssh/authorized_keys
+    Call this with fab -H <hostname> TASK to deploy THREDDS
+    Note: SSH public key must be in /home/thredds/.ssh/authorized_keys
 """
 
 env.user = "thredds"
@@ -14,28 +14,33 @@ code_dir = "/home/thredds/maracoos_catalog"
 prod_dir = "/var/tomcat/THREDDS_PROD"
 
 def deploy_tomcat():
-	git_pull()
-	copy_catalog()
-	restart_tomcat()
+    git_pull()
+    copy_catalog()
+    copy_styles()
+    restart_tomcat()
 
 def git_pull():
-	if not os.path.exists(code_dir):
-		git_clone()
-	with cd(code_dir):
-		run("git pull origin master")
+    if not os.path.exists(code_dir):
+        git_clone()
+    with cd(code_dir):
+        run("git pull origin master")
 
 def git_clone():
-	with cd("~"):
-		run("rm -rf maracoos_catalog")
-		run("git clone https://github.com/asascience-open/maracoos_catalog.git")
+    with cd("~"):
+        run("rm -rf maracoos_catalog")
+        run("git clone https://github.com/asascience-open/maracoos_catalog.git")
 
 def copy_catalog():
-	sudo("rsync -av %s/TDS/* %s/content/thredds/" % (code_dir, prod_dir))
-	sudo("chown -R tomcat:tomcat /var/tomcat/THREDDS_PROD/content/thredds/*")
+    sudo("rsync -av %s/TDS/* %s/content/thredds/" % (code_dir, prod_dir))
+    sudo("chown -R tomcat:tomcat %s/content/thredds/*" % prod_dir)
+
+def copy_styles():
+    sudo("rsync -av %s/thredds_styles/* %s/webapps/thredds/" % (code_dir, prod_dir))
+    sudo("chown -R tomcat:tomcat %s/webapps/thredds/*" % prod_dir)
 
 def restart_tomcat():
-	sudo("service tomcat_thredds restart")
+    sudo("service tomcat_thredds restart")
 
 # Usually this is all that needs to be called
 def deploy():
-	execute(deploy_tomcat)
+    execute(deploy_tomcat)
